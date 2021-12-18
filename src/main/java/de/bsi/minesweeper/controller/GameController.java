f59d0aab3215a4018f2a266f449849a69ea4f357
Elmar Brauch <elmar.brauch@telekom.de>
2021-12-18 16:15:31 +0100

    Every browser session has its own game.

--- src/main/java/de/bsi/minesweeper/controller/GameController.java
+++ src/main/java/de/bsi/minesweeper/controller/GameController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.annotation.SessionScope;
 
 import de.bsi.minesweeper.model.Game;
 import de.bsi.minesweeper.model.Level;
@@ -13,6 +14,7 @@ import de.bsi.minesweeper.model.Position;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@SessionScope
 @RequestMapping("/")
 @Slf4j
 public class GameController {
