2177c970f002f30cc64208613e510fa8f83ef6aa
Elmar Brauch <elmar.brauch@telekom.de>
2022-02-01 17:18:00 +0100

    Possible solution. Further  improvements about Clean Code in JUnit tests are not done yet.

--- src/main/java/de/bsi/minesweeper/service/StatisticService.java
+++ src/main/java/de/bsi/minesweeper/service/StatisticService.java
@@ -1,11 +1,17 @@
 package de.bsi.minesweeper.service;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import de.bsi.minesweeper.model.Game;
+import de.bsi.minesweeper.model.GameStatus;
 
 @Service
 public class StatisticService {
@@ -14,6 +20,59 @@ public class StatisticService {
 	
 	public void addGame(String playerName, Game game) {
 		game2PlayerMap.put(game, playerName);
+	}
+
+	public long ongoingGamesCount() {
+		return game2PlayerMap.keySet().stream()
+				.filter(g -> GameStatus.ONGOING.equals(g.getStatus()))
+				.count();
+	}
+
+	public Optional<String> getPlayerWithMostGamesIn(GameStatus status) {
+		if (status == null)
+			throw new IllegalArgumentException("status must not be null");
+		var groups = game2PlayerMap.entrySet().stream()
+				.filter(e -> status.equals(e.getKey().getStatus()))
+				.collect(Collectors.groupingBy(Entry::getValue));
+		return groups.entrySet().stream()
+				.max((e1, e2) -> e1.getValue().size() - e2.getValue().size())
+				.map(Entry::getKey);
+	}
+
+	double calculateWinRatioFor(String playerName) {
+		long wins = countGamesBy(playerName, GameStatus.WIN);
+		long total = wins + countGamesBy(playerName, GameStatus.LOSE) 
+				+ countGamesBy(playerName, GameStatus.ONGOING);
+		if (total <= 0)
+			return 0.0d;
+		return ((double) wins) / total;
+	}
+
+	private long countGamesBy(String playerName, GameStatus status) {
+		return game2PlayerMap.entrySet().stream()
+				.filter(entry -> status.equals(entry.getKey().getStatus())
+						&& playerName.equals(entry.getValue()))
+				.count();
+	}
+
+	private Comparator<Map.Entry<String, List<Map.Entry<Game, String>>>> winRatioComparator = (e1, e2) -> {
+		double p1WinRatio = calculateWinRatioFor(e1.getKey());
+		double p2WinRatio = calculateWinRatioFor(e2.getKey());
+		if (p1WinRatio == p2WinRatio)
+			return 0;
+		if (p1WinRatio > p2WinRatio)
+			return -1;
+		return 1;
+	};
+	
+	public List<String> top3Players() {
+		return game2PlayerMap.entrySet().stream()
+				.collect(Collectors.groupingBy(Entry::getValue))
+				.entrySet().stream()
+				.sorted(winRatioComparator)
+				.limit(3)
+				.map(Entry::getKey)
+				.toList();
 	}
 
 }
--- src/test/java/de/bsi/minesweeper/service/StatisticServiceTest.java
+++ src/test/java/de/bsi/minesweeper/service/StatisticServiceTest.java
@@ -1,5 +1,11 @@
 package de.bsi.minesweeper.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import de.bsi.minesweeper.model.Game;
@@ -24,9 +30,89 @@ class StatisticServiceTest {
 	@Test
 	void ongoingGamesCount() {
 		startGamesFor(3, PLAYER_1, GameStatus.ONGOING);
+		startGamesFor(3, PLAYER_1, GameStatus.LOSE);
+		assertEquals(3, stats.ongoingGamesCount());
 		startGamesFor(7, PLAYER_2, GameStatus.ONGOING);
-		// TODO Uncomment next line and start doing TDD.
-		// assertEquals(10, stats.ongoingGamesCount());
+		assertEquals(10, stats.ongoingGamesCount());
+	}
+	
+	@Test
+	void getPlayerWithMostOngoingGames() {
+		assertTrue(stats.getPlayerWithMostGamesIn(GameStatus.ONGOING).isEmpty());
+		startGamesFor(3, PLAYER_1, GameStatus.ONGOING);
+		startGamesFor(2, PLAYER_1, GameStatus.LOSE);
+		startGamesFor(4, PLAYER_2, GameStatus.ONGOING);
+		assertEquals(PLAYER_2, stats.getPlayerWithMostGamesIn(GameStatus.ONGOING).get());
+	}
+	
+	@Test
+	void getPlayerWithMostLostGames() {
+		startGamesFor(2, PLAYER_1, GameStatus.LOSE);
+		assertEquals(PLAYER_1, stats.getPlayerWithMostGamesIn(GameStatus.LOSE).get());
+	}
+	
+	@Test
+	void getPlayerWithMostWinGames() {
+		startGamesFor(2, PLAYER_1, GameStatus.WIN);
+		startGamesFor(3, PLAYER_2, GameStatus.WIN);
+		assertEquals(PLAYER_2, stats.getPlayerWithMostGamesIn(GameStatus.WIN).get());
+		startGamesFor(2, PLAYER_1, GameStatus.WIN);
+		assertEquals(PLAYER_1, stats.getPlayerWithMostGamesIn(GameStatus.WIN).get());
+	}
+	
+	@Test
+	void getPlayerWithMostGamesInExceptionTest() {
+		startGamesFor(2, PLAYER_1, GameStatus.LOSE);
+		assertThrows(IllegalArgumentException.class, () -> stats.getPlayerWithMostGamesIn(null)); 
+	}
+	
+	@Test
+	void calculateWinRatioForPlayer() {
+		assertEquals(0, stats.calculateWinRatioFor(PLAYER_1));
+		
+		startGamesFor(2, PLAYER_1, GameStatus.WIN);
+		assertEquals(1, stats.calculateWinRatioFor(PLAYER_1));
+		
+		startGamesFor(2, PLAYER_1, GameStatus.LOSE);
+		assertEquals(0.5, stats.calculateWinRatioFor(PLAYER_1));
+		
+		startGamesFor(20, PLAYER_2, GameStatus.LOSE);
+		startGamesFor(2, PLAYER_1, GameStatus.ONGOING);
+		startGamesFor(2, PLAYER_1, GameStatus.LOSE);
+		assertEquals(0.25, stats.calculateWinRatioFor(PLAYER_1));
+	}
+	
+	@Test
+	void top3Players() {
+		String playerX = "X";
+		String playerY = "Y";
+		
+		startGamesFor(1, playerY, GameStatus.LOSE);
+		assertEquals(List.of(playerY), stats.top3Players());
+		
+		startGamesFor(100, PLAYER_1, GameStatus.WIN);
+		startGamesFor(10, PLAYER_1, GameStatus.LOSE);
+		
+		startGamesFor(1, PLAYER_2, GameStatus.WIN);
+		
+		assertEquals(List.of(PLAYER_2, PLAYER_1, playerY), stats.top3Players());
+		
+		startGamesFor(200, playerX, GameStatus.WIN);
+		startGamesFor(25, playerX, GameStatus.LOSE);
+		
+		assertEquals(List.of(PLAYER_2, PLAYER_1, playerX), stats.top3Players());
+	}
+	
+	@Test
+	void top3PlayersSameWinRatio() {
+		String playerX = "X";
+		
+		startGamesFor(1, PLAYER_1, GameStatus.WIN);
+		startGamesFor(1, PLAYER_2, GameStatus.WIN);
+		startGamesFor(1, playerX, GameStatus.WIN);
+		
+		var topList = stats.top3Players();
+		assertTrue(topList.contains(playerX) && topList.contains(PLAYER_1) && topList.contains(PLAYER_2));
 	}
 	
 	private void startGamesFor(int games, String player, GameStatus status) {
